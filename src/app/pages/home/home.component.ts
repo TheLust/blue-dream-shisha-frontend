ecb1b4ab636afb6e25fc2ef7eb1b487806cab531
Daniel <daniel.ungurean2@gmail.com>
2024-11-13 19:32:36 +0200

    Adapted FE swiper for new public swiper api TODO: fix style

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,39 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
+import { NgForOf, NgIf, NgOptimizedImage } from '@angular/common';
+import { PublicSwiperService, SwiperImageDto } from '../../api';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    NgForOf,
+    NgIf
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  public swiperImages: Array<SwiperImageDto> = [];
+
+  constructor(private publicSwiperService: PublicSwiperService) {
+  }
+
+  ngOnInit(): void {
+    this.publicSwiperService.findSwiperImages()
+      .subscribe({
+        next: (value: Array<SwiperImageDto>) => this.swiperImages = value
+      });
+
+    this.swiperImages.sort((a, b) => (
+      parseInt(a.index || '0') - parseInt(b.index || '0')
+    ));
+  }
+
+  public getImageUrl(swiperImageId: string): string {
+    const baseUrl = this.publicSwiperService.configuration.basePath;
+    return baseUrl + '/api/v1/public/swiper/images/' + swiperImageId;
+  }
 }
